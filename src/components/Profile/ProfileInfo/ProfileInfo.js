382a337bf742928eccca31542e81e8b4b677b31b
HeavyMetalMF <condrashov20123@mail.ru>
2021-10-26 21:49:11 +0300

    lesson 84

--- src/components/Profile/ProfileInfo/ProfileInfo.js
+++ src/components/Profile/ProfileInfo/ProfileInfo.js
@@ -2,6 +2,7 @@ import React from "react";
 import c from './ProfileInfo.module.css';
 import Preloader from "../../common/preloader/Preloader";
 import ProfileStatus from "./ProfileStatus";
+import ProfileStatusHooks from "./ProfileStatusHooks";
 
 const ProfileInfo = (props) => {
     console.log(props.profile)
@@ -17,7 +18,7 @@ const ProfileInfo = (props) => {
                 <img className={c.ava}
                      src={props.profile.photos.large}/>
             </div>
-            <ProfileStatus status={props.status} updateStatus={props.updateStatus}/>
+            <ProfileStatusHooks status={props.status} updateStatus={props.updateStatus}/>
         </div>
     );
 }
---
+++ src/components/Profile/ProfileInfo/ProfileStatusHooks.js
@@ -0,0 +1,40 @@
+import React, {useState} from "react";
+import c from './ProfileInfo.module.css';
+import Preloader from "../../common/preloader/Preloader";
+
+const ProfileStatusHooks = (props) => {
+    debugger
+    let [editMode, setEditMode] = useState(false);
+    let [status, setStatus] = useState(props.status);
+
+    const activateEditMode = () => {
+        setEditMode(true);
+    }
+
+    const deactivateEditMode = () =>  {
+        setEditMode(false);
+        props.updateStatus(status)
+    }
+
+    const onStatusChange = (e) => {
+        setStatus(e.currentTarget.value);
+    }
+
+    return (
+        <div>
+            {!editMode &&
+            <div>
+                <span onDoubleClick={activateEditMode} >{props.status || 'No status' }</span>
+            </div>
+            }
+
+            {editMode &&
+            <div>
+                <input autoFocus onBlur={deactivateEditMode} onChange={onStatusChange} value={status}
+                       />
+            </div>
+            }
+        </div>
+    )
+}
+export default ProfileStatusHooks;
